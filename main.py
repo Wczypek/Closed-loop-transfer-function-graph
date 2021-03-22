26902e47a2b91cd5d9c72018a5ea6becc86dbbe1
Wczypek <66012155+Wczypek@users.noreply.github.com>
2021-03-22 14:17:31 +0100

    dodanie funkcji
    funkcja całkowania oraz funkcja rysowania wykresu
    zmieniłem też operacje wewnątrz pętli taylora

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 t1 = 50
-l=2.5
-h=0.001
-kroki=int(t1/h)
+l = 2.5
+h = 0.001
+kroki = int(t1 / h)
 
 amp = 1
 a = 0.5
@@ -14,26 +14,26 @@ m = 0.5
 
 t = np.zeros(kroki)
 for x in range(kroki):
-    t[x] = x*h
+    t[x] = x * h
 
-u = amp * np.sign(np.sin(2*np.pi*t/(t1/l)))
+u = amp * np.sign(np.sin(2 * np.pi * t / (t1 / l)))
 
-Y2p =  np.zeros(kroki)
-Y1p =  np.zeros(kroki)
-Y =  np.zeros(kroki)
-#warunki poczatkowe
+def calkowanie():
+    #warunki początkowe
+    Y2p = []
+    Y1p = [0]
+    Y = [0]
 
-Y2p[0] = 0
-Y1p[0] = 0
-Y[0] = 0
+    for i in range(kroki - 1):
+        Y2p.append(-Y1p[i] * a - Y[i] * k * b * m + u[i] * k * b)
+        Y1p.append(Y1p[i] + h / 1 * Y2p[i])
+        Y.append(Y[i] + h / 1 * Y1p[i] + h * h / 2 * Y2p[i])
+    return Y
 
+def rysowanie_wykresu():
+    calkowanie()
+    plt.plot(t, calkowanie())
+    plt.plot(t, u)
+    plt.show()
 
-for i in range(kroki-1):
-    Y2p[i] = -Y1p[i] * a - Y[i] * k * b * m + u[i] * k * b
-    Y1p[i+1] = Y1p[i] + h/1 *Y2p[i]
-    Y[i+1] = Y[i] + h/1 * Y1p[i] + h*h/2 * Y2p[i]
-
-
-plt.plot(t, Y)
-plt.plot(t, u)
-plt.show()
+rysowanie_wykresu()
